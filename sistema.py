8b2aabacc1463782b1bfc867aff17aebd41dfb54
Jonas <vjonaseloy@gmail.com>
2026-09-23 19:58:28 -0300

    class conexion añadida

--- sistema.py
+++ sistema.py
@@ -4,3 +4,11 @@ from tabulate import tabulate
 #comentario x jijoooo
 
 class conexion:
+        
+    def __init__(self):
+        self.host = "localhost"
+        self.usuario = "root"
+        self.password = "root124"  # pone tu contraseña acá
+        self.base_datos = "ABP_bytefix"
+        self.conexion = None
+        self.cursor = None
