5e48fe5c402605785ac738bdac81580ed8c6af82
Jonas <vjonaseloy@gmail.com>
2026-09-24 13:24:04 -0300

    Parte del menu principal echa,  funcines auxiliares para el menu echas, base para luego hacer la funcion de gestion echa.

--- sistema.py
+++ sistema.py
@@ -1,10 +1,11 @@
 import mysql.connector
+import os
 from tabulate import tabulate
 
 
 #comentario x jijoooo
 
-class conexion:
+class Conexion:
     def __init__(self):
         self.conexion = mysql.connector.connect(
             host="localhost", user="root", password="root124", database="ABP_bytefix"
@@ -23,8 +24,44 @@ class conexion:
         self.cursor.close()
         self.conexion.close()
 
+# Funciones auxiliares (principalmente para el menu)
 
-#hola mi amor
+def limpiar(): # la bosta esta hace que el menu en la terminal se limpie y solo muestre el menu actual.
+    os.system("cls" if os.name == "nt" else "clear") 
+
+def pausar(): # esto solo muestra un mensaje y al pulsar cualquier tecla o enter la funcion termina y el programa sigue.
+    input("\n Presiona cualquier tecla para continuar...") # en resumen solo le da tiempo al usuario para leer lo que se mostro antes de que el programa siga, sino se borra la pantalla xd.
+
+def mostrar(filas, headers):
+    print(tabulate(filas, headers=headers, tablefmt="rounded_grid") if filas else "Sin registros.")
+    pausar()
+
+# Funcion generica que gestiona las entidades: basicamente lee la opcion que elige el usuario y ejecuta la operacion que corresponda, tmb se ejecuta hasta que el ciclo se corta.
+
+def gestionar(entidad, headers, busquedas ): #parametros q luego usare jijo
+
+# Menu principal
+
+def Main():
+    db = Conexion()
+    while True:
+        limpiar()
+        print("=" * 50)
+        print("   SISTEMA DE GESTIÓN    ")
+        print("=" * 50)
+        print("1. Productos")
+        print("2. Clientes")
+        print("3. Empleados")
+        print("4. Reparaciones")
+        print("5. Transacciones")
+        print("6. Reporte: Reparaciones pendientes (vista)")
+        print("0. Salir")
+        print("=" * 50)
+        op = input("Opción: ")
+        
+        #if op == "1":
+            ###
+            
 
 ""
 """
